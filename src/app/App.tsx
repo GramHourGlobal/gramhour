419417b5e052c73e5788843e97825b20d826a76b
GramHourGlobal <gramhourglobal@gmail.com>
2026-05-09 19:41:38 +0530

    Update App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,19 @@
+import { useEffect, useState } from "react";
 import MobileApp from "./MobileApp";
 import DesktopApp from "./DesktopApp";
 
 export default function App() {
-  const isDesktop = window.innerWidth >= 1024;
+  const [isDesktop, setIsDesktop] = useState(window.innerWidth >= 1024);
 
-  return (
-    <>
-      {isDesktop ? <DesktopApp /> : <MobileApp />}
-    </>
-  );
+  useEffect(() => {
+    const handleResize = () => {
+      setIsDesktop(window.innerWidth >= 1024);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return isDesktop ? <DesktopApp /> : <MobileApp />;
 }
