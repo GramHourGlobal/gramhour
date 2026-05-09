a3183f328291c800221f440e9e183ed941953b0c
GramHourGlobal <gramhourglobal@gmail.com>
2026-05-09 18:34:20 +0530

    Update App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,1 +1,12 @@
+import MobileApp from "./MobileApp";
+import DesktopApp from "./DesktopApp";
 
+export default function App() {
+  const isDesktop = window.innerWidth >= 1024;
+
+  return (
+    <>
+      {isDesktop ? <DesktopApp /> : <MobileApp />}
+    </>
+  );
+}
