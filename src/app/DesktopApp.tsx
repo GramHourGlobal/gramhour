3ff719cc5817787d4aeb27daa4b3cedcbab34f84
GramHourGlobal <gramhourglobal@gmail.com>
2026-05-09 23:15:16 +0530

    Update DesktopApp.tsx

--- src/app/DesktopApp.tsx
+++ src/app/DesktopApp.tsx
@@ -1509,16 +1509,6 @@ export default function DesktopApp() {
                   >
                     Why Choose Us
                   </button>
-                 <button
-  onClick={() => {
-    document.getElementById('why-gramhour')
-      ?.scrollIntoView({ behavior: 'smooth' });
-  }}
-  className="text-white hover:text-[#C4943A] transition-colors text-[15px]"
-  style={{ fontFamily: 'DM Sans, sans-serif' }}
->
-  Why Choose Us
-</button>
                 </div>
               </div>
 
