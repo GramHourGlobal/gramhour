d8c83a447ac619a244516e0384ec0df5d29f78b3
GramhourGlobal <gramhourglobal@gmail.com>
2026-05-22 18:15:58 +0530

    Lenis scroll

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,37 @@
 
-  import { createRoot } from "react-dom/client";
+import { createRoot } from "react-dom/client";
+import { useEffect } from "react";
+import Lenis from "@studio-freight/lenis";
 import App from "./app/App.tsx";
 import "./styles/index.css";
 
+function SmoothScroll({ children }: { children: React.ReactNode }) {
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.4,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      orientation: "vertical",
+      smoothWheel: true,
+      wheelMultiplier: 0.8,
+      touchMultiplier: 1.5,
+    });
+
+    function raf(time: number) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+
+    return () => lenis.destroy();
+  }, []);
+
+  return <>{children}</>;
+}
+
 createRoot(document.getElementById("root")!).render(
-  <App />
+  <SmoothScroll>
+    <App />
+  </SmoothScroll>
 );
   
